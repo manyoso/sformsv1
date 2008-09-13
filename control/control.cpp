0f886459e2593f63688a716e85669df66f747459
Adam Treat <treat@kde.org>
2008-09-13 19:42:04 -0400

    * Some warnings and checks to make sure we start in the right order

--- control/control.cpp
+++ control/control.cpp
@@ -5,6 +5,7 @@
 #include <QAction>
 #include <QProcess>
 #include <QGroupBox>
+#include <QMessageBox>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
 #include <QToolButton>
@@ -21,12 +22,14 @@ Control::Control(QWidget *parent)
     connect(_actionReaperStart, SIGNAL(triggered()), this, SLOT(startReaper()));
 
     _actionReaperStop = new QAction(tr("&Stop"), this);
+    _actionReaperStop->setEnabled(false);
     connect(_actionReaperStop, SIGNAL(triggered()), this, SLOT(stopReaper()));
 
     _actionGenesisStart = new QAction(tr("&Start"), this);
     connect(_actionGenesisStart, SIGNAL(triggered()), this, SLOT(startGenesis()));
 
     _actionGenesisStop = new QAction(tr("&Stop"), this);
+    _actionGenesisStop->setEnabled(false);
     connect(_actionGenesisStop, SIGNAL(triggered()), this, SLOT(stopGenesis()));
 
     QWidget *centralWidget = new QWidget(this);
@@ -70,22 +73,38 @@ Control::~Control()
 
 void Control::startReaper()
 {
-    if (!_reaper) {
-        QString command = QCoreApplication::applicationDirPath() +
-                          QDir::separator() + QLatin1String("reaper");
-        _reaper = new QProcess(this);
-        _reaper->setWorkingDirectory(QCoreApplication::applicationDirPath());
-        _reaper->setProcessChannelMode(QProcess::ForwardedChannels);
-        _reaper->start(command);
-    }
+    if (_reaper)
+        return;
 
     qDebug() << "startReaper";
+    QString command = QCoreApplication::applicationDirPath() +
+                      QDir::separator() + QLatin1String("reaper");
+    _reaper = new QProcess(this);
+    _reaper->setWorkingDirectory(QCoreApplication::applicationDirPath());
+    _reaper->setProcessChannelMode(QProcess::ForwardedChannels);
+    _reaper->start(command);
+
+    _actionReaperStart->setEnabled(false);
+    _actionReaperStop->setEnabled(true);
 }
 
 void Control::stopReaper()
 {
     if (!_reaper)
         return;
+
+    if (_genesis) {
+        QMessageBox::StandardButton button = QMessageBox::question(this,
+            tr("Stop Reaper?"),
+            tr("<b>Warning:</b> Genesis is still running! <p>Stopping Reaper"
+                " with Genesis still running can lead to runaway SForm reproduction"
+                " and process creation. This can crash the computer.</p>"
+                " <p>Really stop Reaper anyway?</p>"),
+            QMessageBox::Yes | QMessageBox::No, QMessageBox::No);
+        if (button != QMessageBox::Yes) {
+            return;
+        }
+    }
 
     qDebug() << "stopReaper";
     _reaper->write("stop");
@@ -94,20 +113,39 @@ void Control::stopReaper()
     _reaper->kill();
     delete _reaper;
     _reaper = 0;
+
+    _actionReaperStart->setEnabled(true);
+    _actionReaperStop->setEnabled(false);
 }
 
 void Control::startGenesis()
 {
-    if (!_genesis) {
-        QString command = QCoreApplication::applicationDirPath() +
-                          QDir::separator() + QLatin1String("genesis");
-        _genesis = new QProcess(this);
-        _genesis->setWorkingDirectory(QCoreApplication::applicationDirPath());
-        _genesis->setProcessChannelMode(QProcess::ForwardedChannels);
-        _genesis->start(command);
+    if (_genesis)
+        return;
+
+    if (!_reaper) {
+        QMessageBox::StandardButton button = QMessageBox::question(this,
+            tr("Start Genesis?"),
+            tr("<b>Warning:</b> Reaper is not running! <p>Starting Genesis"
+                " without Reaper running can lead to runaway SForm reproduction"
+                " and process creation. This can crash the computer.</p>"
+                " <p>Really start Genesis anyway?</p>"),
+            QMessageBox::Yes | QMessageBox::No, QMessageBox::No);
+        if (button != QMessageBox::Yes) {
+            return;
+        }
     }
 
     qDebug() << "startGenesis";
+    QString command = QCoreApplication::applicationDirPath() +
+                      QDir::separator() + QLatin1String("genesis");
+    _genesis = new QProcess(this);
+    _genesis->setWorkingDirectory(QCoreApplication::applicationDirPath());
+    _genesis->setProcessChannelMode(QProcess::ForwardedChannels);
+    _genesis->start(command);
+
+    _actionGenesisStart->setEnabled(false);
+    _actionGenesisStop->setEnabled(true);
 }
 
 void Control::stopGenesis()
@@ -122,4 +160,7 @@ void Control::stopGenesis()
     _genesis->kill();
     delete _genesis;
     _genesis = 0;
+
+    _actionGenesisStart->setEnabled(true);
+    _actionGenesisStop->setEnabled(false);
 }
