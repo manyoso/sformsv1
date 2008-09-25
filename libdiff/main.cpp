7bd76c9fb4e08104d385b30c3b025cc69e495945
Adam Treat <treat@kde.org>
2008-09-24 23:32:29 -0400

    * Add line based LCS

--- libdiff/main.cpp
+++ libdiff/main.cpp
@@ -45,6 +45,51 @@ private:
     QVector<int> m_vector;
 };
 
+int lcs_lengthLines(const QByteArray &a, const QByteArray &b)
+{
+    QList<QByteArray> aLines = a.split('\n');
+    QList<QByteArray> bLines = b.split('\n');
+    const int aSize = aLines.size();
+    const int bSize = bLines.size();
+
+    LCSLengths l(aSize, bSize);
+
+    for (int i = aSize - 1; i >= 0; i--) {
+        for (int j = bSize - 1; j >= 0; j--) {
+            if (aLines.at(i) == bLines.at(j)) {
+                l.setValue(i, j, 1 + l.value(i+1, j+1));
+            } else {
+                l.setValue(i, j, qMax(l.value(i+1, j), l.value(i, j+1)));
+            }
+        }
+    }
+
+    //l.print();
+
+    const int length = l.value(0, 0);
+
+    QList<QByteArray> longestSubSequence;
+    int i = 0;
+    int j = 0;
+    while (i < aSize && j < bSize) {
+        if (aLines.at(i) == bLines.at(j)) {
+            longestSubSequence.append(aLines.at(i));
+            i++; j++;
+        } else if (l.value(i+1, j) >= l.value(i, j+1)) {
+            i++;
+        } else {
+            j++;
+        }
+    }
+
+    qDebug() << "longestSubSequence";
+    foreach (QByteArray line, longestSubSequence) {
+        qDebug() << line;
+    }
+
+    return length;
+}
+
 int lcs_length(const QByteArray &a, const QByteArray &b)
 {
     const int aSize = a.size();
@@ -87,24 +132,70 @@ int lcs_length(const QByteArray &a, const QByteArray &b)
 
 int main(int, char **)
 {
-#if 1
+#if 0
     QString a("nematode knowledge");
     QString b("empty bottle");
 
     //emt ole is the solution...
-#else
-//     QString a("XMJYAUZ");
-//     QString b("MZJAWXU");
-//
-//     //MJAU is the solution...
-
-    QString a("abcdfghjqz");
-    QString b("abcdefgijkrxyz");
-
-    //abcdfgjz is the solution...
-#endif
-
     qDebug() << "length" << lcs_length(a.toLatin1(), b.toLatin1());
+#else
+
+    QString a("This part of the\n"
+              "document has stayed the\n"
+              "same from version to\n"
+              "version.  It shouldn't\n"
+              "be shown if it doesn't\n"
+              "change.  Otherwise, that\n"
+              "would not be helping to\n"
+              "compress the size of the\n"
+              "changes.\n"
+              "\n"
+              "This paragraph contains\n"
+              "text that is outdated.\n"
+              "It will be deleted in the\n"
+              "near future.\n"
+              "\n"
+              "It is important to spell\n"
+              "check this dokument. On\n"
+              "the other hand, a\n"
+              "misspelled word isn't\n"
+              "the end of the world.\n"
+              "Nothing in the rest of\n"
+              "this paragraph needs to\n"
+              "be changed. Things can\n"
+              "be added after it.\n");
+
+    QString b("This is an important\n"
+              "notice! It should\n"
+              "therefore be located at\n"
+              "the beginning of this\n"
+              "document!\n"
+              "\n"
+              "This part of the\n"
+              "document has stayed the\n"
+              "same from version to\n"
+              "version.  It shouldn't\n"
+              "be shown if it doesn't\n"
+              "change.  Otherwise, that\n"
+              "would not be helping to\n"
+              "compress anything.\n"
+              "\n"
+              "It is important to spell\n"
+              "check this document. On\n"
+              "the other hand, a\n"
+              "misspelled word isn't\n"
+              "the end of the world.\n"
+              "Nothing in the rest of\n"
+              "this paragraph needs to\n"
+              "be changed. Things can\n"
+              "be added after it.\n"
+              "\n"
+              "This paragraph contains\n"
+              "important new additions\n"
+              "to this document.\n");
+
+    qDebug() << "length" << lcs_lengthLines(a.toLatin1(), b.toLatin1());
+#endif
 
     return 0;
 }
